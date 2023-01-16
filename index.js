38a5f24dc7b985c3ff6638df261c3f276da2e875
Joel John <joeljohn5112@gmail.com>
2023-01-16 16:40:02 +0530

    formatted using prettier

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
 // Dice 1
 
-var randomNumber1 = Math.floor((Math.random()*6) + 1);
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
 
-if(randomNumber1 === 1){
-    document.querySelectorAll("img")[0].setAttribute("src","images/dice1.png");
-}
-else if(randomNumber1 === 2){
-    document.querySelectorAll("img")[0].setAttribute("src","images/dice2.png");
-}
-else if(randomNumber1 === 3){
-    document.querySelectorAll("img")[0].setAttribute("src","images/dice3.png");
-}
-else if(randomNumber1 === 4){
-    document.querySelectorAll("img")[0].setAttribute("src","images/dice4.png");
-}
-else if(randomNumber1 === 5){
-    document.querySelectorAll("img")[0].setAttribute("src","images/dice5.png");
-}
-else if(randomNumber1 === 6){
-    document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png");
-}
+var RandomDiceImageSource1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", RandomDiceImageSource1);
 
 // Dice 2
-var randomNumber2 = Math.floor((Math.random()*6) + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
-if(randomNumber2 === 1){
-    document.querySelectorAll("img")[1].setAttribute("src","images/dice1.png");
-}
-else if(randomNumber2 === 2){
-    document.querySelectorAll("img")[1].setAttribute("src","images/dice2.png");
-}
-else if(randomNumber2 === 3){
-    document.querySelectorAll("img")[1].setAttribute("src","images/dice3.png");
-}
-else if(randomNumber2 === 4){
-    document.querySelectorAll("img")[1].setAttribute("src","images/dice4.png");
-}
-else if(randomNumber2 === 5){
-    document.querySelectorAll("img")[1].setAttribute("src","images/dice5.png");
-}
-else if(randomNumber2 === 6){
-    document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png");
-}
+var RandomDiceImageSource2 = "images/dice" + randomNumber2 + ".png";
 
+document.querySelectorAll("img")[1].setAttribute("src", RandomDiceImageSource2);
+
+// Choosing the winner
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "⛳Player1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "⛳Player2 Wins";
+} else {
+  document.querySelector("h1").innerHTML = "Draw";
+}
